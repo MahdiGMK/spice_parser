541324137ea9e7eb9c69b2366e8b661824158ba4
MahdiGMK <mbahramiand@gmail.com>
2025-04-05 17:02:24 +0330

    parasitic res/cap

--- spice_parser.py
+++ spice_parser.py
@@ -1,24 +1,28 @@
+from ast import Compare
+
+
 class Component:
     def __init__(self, spl:list):
-        self.parts = spl
         self.name = spl[0]
         if(self.name[0] == 'V'):
             self.wires = spl[1:3]
-            self.Vdef = spl[3:]
+            self.Mdef = spl[3:]
         elif(self.name[0] == 'R'):
             self.wires = spl[1:3]
-            self.Rdef = spl[3:]
+            self.Mdef = spl[3:]
         elif(self.name[0] == 'C'):
             self.wires = spl[1:3]
-            self.Cdef = spl[3:]
+            self.Mdef = spl[3:]
         elif(self.name[0] == 'M'):
             self.wires = spl[1:5]
             self.Mdef = spl[5:]
         elif(self.name[0] == 'X'):
             self.wires = spl[1:-1]
-            self.Xdef = spl[-1:]
+            self.Mdef = spl[-1:]
         else:
             raise ValueError('Invalid Component')
+    def __str__(self) -> str:
+        return f'{self.name} {' '.join(self.wires)} {' '.join(self.Mdef)}'
 
 
 # Rtest = "Rs_M4 Vss Vss_par_M4 0.1"
@@ -44,6 +48,11 @@ class SubCkt:
         self.name = name
         self.ports = ports
         self.body: CompList = []
+    def __str__(self) -> str:
+        return f'''
+.SUBCKT {self.name} {' '.join(self.ports)}
+{'\n'.join([str(comp) for comp in self.body])}
+.ENDS '''
 class Circuit:
     def __init__(self):
         self.title = ""
@@ -51,6 +60,13 @@ class Circuit:
         self.models:list[list[str]] = []
         self.body:TokList = []
         self.subckts: list[SubCkt] = []
+    def __str__(self) -> str:
+        return (f'''
+.TITLE {self.title}
+.GLOBAL {' '.join(self.globals)}
+{'\n'.join([f'.MODEL {' '.join(m)}' for m in self.models])}
+{'\n'.join([str(subckt) for subckt in self.subckts])}
+{'\n'.join([str(tok) for tok in self.body])} ''')
 
 def tok_get_name(tok: Tok):
     pass
@@ -63,7 +79,9 @@ def readSpiceNetlist(file_path) -> TokList:
             if not line or line.startswith("*"): continue
             spl = line.split()
             if spl[0] == '.include':
-                for subins in readSpiceNetlist(spl[1]): # naive preproc
+                import os
+                incpath = os.path.dirname(file_path)+'/'+spl[1]
+                for subins in readSpiceNetlist(incpath): # naive preproc
                     res.append(subins)
             else:
                 res.append(Tok(spl))
@@ -83,23 +101,44 @@ def toCircuit(toklist: TokList):
         elif parts[0].upper() == '.SUBCKT':
             curSubCkt = SubCkt(parts[1], parts[2:])
         elif parts[0].upper() == '.ENDS':
-            if(curSubCkt):
+            if curSubCkt:
                 res.subckts.append(curSubCkt)
             curSubCkt = None
-        # elif parts[0].upper() == '.TRAN':
-        #     pass
-        # elif parts[0].upper() == '.CONTROL':
-        #     pass
-        # elif parts[0].upper() == '.ENDC':
-        #     pass
-
-
+        elif curSubCkt:
+            comp = tok.asComp()
+            if comp:
+                curSubCkt.body.append(comp)
+            else:
+                raise ValueError("Invalid Component inside SubCkt")
+        else:
+            res.body.append(tok)
     return res
 
-def toSpiceNetlist(toklist: TokList):
-    return '\n'.join([str(tok) for tok in toklist])
+def addParasiticResistance(subckt: SubCkt, res = 0.1):
+    allwires = []
+    new_comp = []
+    for comp in subckt.body:
+        before_res = comp.wires
+        after_res = []
+        for w in before_res:
+            res_name = f'R_gen_{comp.name}__{len(after_res)}'
+            new_comp.append(Component([res_name , res_name , w , str(res)]))
+            after_res.append(res_name)
+        comp.wires = after_res
+    for comp in new_comp:
+        subckt.body.append(comp)
 
-toklist = readSpiceNetlist('./counter_tb_spice.cir')
+def addParasiticCapacitance(subckt: SubCkt, cap = '0.1p'):
+    allwires = []
+    for comp in subckt.body:
+        for w in comp.wires:
+            allwires.append(w)
+    for x in set(allwires):
+        subckt.body.append(Component([f'C_gen_{x}' , '0' , x , str(cap)]))
+
+toklist = readSpiceNetlist('../counter_tb_spice.cir')
 circ = toCircuit(toklist)
-netlist = toSpiceNetlist(toklist)
-print(netlist)
+for subckt in circ.subckts:
+    addParasiticResistance(subckt)
+    addParasiticCapacitance(subckt)
+print(circ)
